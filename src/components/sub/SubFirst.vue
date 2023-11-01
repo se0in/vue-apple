bd310ac33d09d1dcd7164f20b1ced30cabbb4a3b
se0in <tjfls0@naver.com>
2023-11-01 18:03:43 +0900

    스크롤이벤트 텍스트 부분 진행 중

--- src/components/sub/SubFirst.vue
+++ src/components/sub/SubFirst.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="rotate-iphone">
-    <div @scroll="handleScroll" @wheel="handleWheel" class="scroll-container" ref="scrollContainer" >
+    <div @scroll="handleScroll" class="scroll-container" ref="scrollContainer" @wheel="handleWheel">
       <div class="scroll_wrap">
-        <img 
-        
-          class="rotating-image" 
-          src="../../assets/img/SubFirst-iphone.png" 
-          alt="iphone" 
-          ref="iphoneImage">
-          <p class="scroll_text" ref="scrollText1" :class="{ 'scrollTextShow': scrollText }">탁월한 선택.</p>
-          <div class="column_text">
-            <ul>
-              <li>쾌속으로 질주하는 프로세서.</li>
-              <li>획기적인 배터리 성능.</li>
-              <li>고속 5G 연결.</li>
-              <li>오랜 사용을 염두에 둔 디자인.</li>
-              <li>수준급 카메라.</li>
-              <li>익숙해서 좋은 홈버튼.</li>
-            </ul>
-          </div>
+        <img class="rotating-image" src="../../assets/img/SubFirst-iphone.png" alt="iphone" ref="iphoneImage"
+          v-if="iphoneImage">
+        <p class="scroll_text" ref="scrollText" :class="{ 'scrollTextShow': scrollText }">탁월한 선택.</p>
+        <div class="column_text">
+          <ul>
+            <li>쾌속으로 질주하는 프로세서.</li>
+            <li>획기적인 배터리 성능.</li>
+            <li>고속 5G 연결.</li>
+            <li>오랜 사용을 염두에 둔 디자인.</li>
+            <li>수준급 카메라.</li>
+            <li>익숙해서 좋은 홈버튼.</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -31,79 +27,71 @@ export default {
       initialImageWidth: 200, // 초기 이미지 너비 설정
       scrollThreshold: 800, // 스크롤 임계값 설정
       maxScrollPosition: 10000, // 최대 스크롤 위치 설정
-      scrollText : false
+      scrollText: false,
+      iphoneImage: true,
+      lastScrollPosition: 0,
     };
   },
   methods: {
-    /*  handleWheel(event) {
-      // 휠 이벤트의 deltaY 값은 마우스 휠의 스크롤 방향과 속도에 따라 변합니다.
-      // 음수 값은 위로 스크롤하고 양수 값은 아래로 스크롤합니다.
-      const deltaY = event.deltaY;
-
-      // 원하는 스크롤 이동값 설정 (예: 50px)
-      // const scrollStep = 50;
-
-      // 스크롤 이동값을 제한 범위로 설정
-      const newScrollPosition = this.$refs.scrollContainer.scrollTop + deltaY;
-      const maxScrollPosition = 500; // 원하는 최대 스크롤 픽셀 값
-
-      // 스크롤 이동값이 제한 범위를 벗어나지 않도록 처리
-      if (newScrollPosition < 0) {
-        this.$refs.scrollContainer.scrollTop = 0;
-      } else if (newScrollPosition > maxScrollPosition) {
-        this.$refs.scrollContainer.scrollTop = maxScrollPosition;
-      } else {
-        this.$refs.scrollContainer.scrollTop = newScrollPosition;
-      }
-
-      // 이벤트 기본 동작을 막아 스크롤 이벤트가 여러 번 발생하는 것을 방지
-      event.preventDefault();
-    }, */
-
+    handleWheel(event) {
+      const scrollContainer = this.$refs.scrollContainer;
+      scrollContainer.scrollTop += event.deltaY > 0 ? 80 : -500;
+    },
 
     handleScroll(event) {
+      const scrollPosition = event.target.scrollTop;
+      const rotatingImage = this.$refs.iphoneImage;
 
+      if (rotatingImage) {
+        if (scrollPosition == 0) {
 
+          this.scrollText = false;
+          rotatingImage.style.transform = `rotate(0deg)`;
+        }
+        else if (scrollPosition < 1600) {
+          const scaleFactor = 1 + scrollPosition / this.scrollThreshold;
+          const imageWidth = this.initialImageWidth * scaleFactor;
+          const rotationDegrees = Math.min(scrollPosition, 90);
+          rotatingImage.style.transform = `rotate(-${rotationDegrees}deg)`;
+          rotatingImage.style.width = `${imageWidth}px`;
+          this.scrollText = true;
+        } else if (scrollPosition == 1600) {
+          this.scrollText = false;
 
-      
-      
-      const scrollPosition = event.target.scrollTop; /* scrollTop */
+        } else if (scrollPosition <= 1900) {
+          this.scrollText = false;
+          const scaleFactor = 1 + scrollPosition / this.scrollThreshold;
+          const imageWidth = this.initialImageWidth * scaleFactor;
+          const rotationDegrees = Math.min(scrollPosition, 90);
+          rotatingImage.style.transform = `rotate(-${rotationDegrees}deg)`;
+          rotatingImage.style.width = `${imageWidth + rotationDegrees}px`;
+        } else if (scrollPosition <= 2400) {
+          this.scrollText = false;
+        } else if (scrollPosition <= 3000) {
+          // const imageWidth = this.initialImageWidth + (scrollPosition - 1000) * 0.1;
+          const rotationDegrees = Math.min(scrollPosition - 1000, 90);
+          rotatingImage.style.transform = `rotate(-${rotationDegrees}deg)`;
+          rotatingImage.style.width = "203vh";
+        }
+      }
       console.log('scrollPosition: ', scrollPosition);
 
-      const rotatingImage = this.$refs.iphoneImage; /* 이미지 */
-      // const scrollText = this.$refs.scrollText1;
-      
-      const scaleFactor = 1 + scrollPosition / this.scrollThreshold;
-      /* 1(필수) + 스크롤탑 / 스크롤임계점 */
-      
-      /* 초기 이미지 * 사진확대 = 이미지 크기 */
-      const imageWidth = this.initialImageWidth * scaleFactor;
-      
-      /* 각도와 scrollTop 동일 , 각도는 90deg이 최대  */
-      
-      const rotationDegrees = Math.min(scrollPosition, 90); /* 각도 */
-      if (scrollPosition <= 200) {
-        /* 글자 안보이게 */
-        this.scrollText = true;
-        rotatingImage.style.width = `${imageWidth}px`;
-        rotatingImage.style.transform = `rotate(-${rotationDegrees}deg) `;
-        rotatingImage.style.transition = "rotate 1s";
+      // 스크롤 방향을 판단하여 이미지 나타남/사라짐 동작을 처리합니다.
+      if (scrollPosition > 3000) {
+        // 스크롤을 아래로 내릴 때
+        rotatingImage.style.opacity = "0"; // 이미지 사라짐
+        rotatingImage.style.display = "none"
+      } else if (scrollPosition == 3000) {
+        rotatingImage.style.display = "none"
 
-
-
-
-
-      }else if (scrollPosition <= 300 ) {
-        this.scrollText = false;
-        rotatingImage.style.transition = "width 1s";
-        rotatingImage.style.width = `100%`;
-      }else {
-        // rotatingImage.style.transform = `rotate(-${rotationDegrees}deg) `;
+      } else {
+        rotatingImage.style.display = "block"
+        // 스크롤을 올릴 때
+        rotatingImage.style.opacity = "1"; // 이미지 나타남
       }
-      
-      
-      
-    
+
+      // 스크롤 위치 업데이트
+      this.lastScrollPosition = scrollPosition;
     },
   },
 };
@@ -111,65 +99,85 @@ export default {
 
 <style lang="scss" scoped>
 .rotate-iphone {
-  // overflow-x: hidden;
-    // width: 100%;
+  overflow-x: hidden;
+  position: relative;
+  width: 100%;
+
+  &::-webkit-scrollbar,
+  &::-webkit-scrollbar-track,
+  &::-webkit-scrollbar-thumb {
+    width: 0;
+  }
+
   .scroll-container {
-    // overflow-x: hidden;
-    // width: 100%;
     height: 100vh;
     overflow-y: scroll;
+
+    &::-webkit-scrollbar,
+    &::-webkit-scrollbar-track,
+    &::-webkit-scrollbar-thumb {
+      width: 0;
+    }
   }
+
   .scroll_wrap {
-    overflow-x: hidden;
-    width: 100%;
-    height: 3000px;
+    height: 9000px;
     background-color: #ccc;
-    // perspective:1000px;
-    // padding-top: 50px;
+
     img.rotating-image {
-      transition: transform 0.5s;
-      max-width: 100%;
+      transition: transform 0.5s, width 0.5s, opacity 0.5s;
       width: 200px;
       position: absolute;
       left: 0;
       right: 0;
-      top: 0;
+      top: -20.8%;
       bottom: 0;
       margin: auto;
-      pointer-events: none; 
-      z-index: 2;
+      pointer-events: none;
+      z-index: 1;
     }
+
     .scroll_text {
       font-size: 50px;
       font-weight: bold;
-      position: relative;
-      // left: 50%;
-      // transform: translateX(-50%);
+      position: fixed;
+      left: 50%;
+      transform: translateX(-50%);
       top: 80vh;
       opacity: 0;
-      transition: opacity .5s;
+      transition: opacity 0.5s;
       text-align: center;
-      z-index: 1;
-      background:linear-gradient(90deg, rgb(46, 29, 123) 30%, rgb(104, 86, 177) 55%);
+      z-index: 2;
+      background: linear-gradient(90deg, rgb(46, 29, 123) 30%, rgb(104, 86, 177) 55%);
       background-clip: text;
       color: transparent;
+
       &.scrollTextShow {
         opacity: 1;
       }
     }
+
     .column_text {
       position: absolute;
       top: 40px;
       width: 100%;
-      background-color: red;
       height: calc(100vh - 40px);
       z-index: 2;
-      display: none;
+      overflow-x: hidden;
+
       ul {
         li {
+          width: 3000px;
           padding-left: 120px;
           font-size: 36px;
           line-height: calc((100vh - 40px) / 6);
+          color: #fff;
+          &:nth-child(1) {background: linear-gradient(90deg, rgb(155, 168, 182) 0%, rgb(24, 14, 78) 35%, rgba(59,41,144,1) 60%, rgba(194,132,107,1) 100%);}
+          &:nth-child(2) {background: linear-gradient(90deg, rgb(165, 182, 192) 0%, rgba(25,25,74,1) 26%, rgb(30, 18, 78) 64%, rgba(179,100,100,1) 100%);}
+          &:nth-child(3) {background: linear-gradient(90deg, rgb(174, 186, 240) 0%, rgb(20, 31, 102) 23%, rgb(61, 37, 145) 70%, rgba(147,74,92,1) 100%);}
+          &:nth-child(4) {background: linear-gradient(90deg, rgb(152, 175, 169) 0%, rgba(40,50,109,1) 23%, rgba(47,29,118,1) 72%, rgba(130,68,91,1) 100%);}
+          &:nth-child(5) {background: linear-gradient(90deg, rgb(168, 187, 173) 0%, rgba(30,30,87,1) 27%, rgba(52,34,129,1) 64%, rgba(148,89,97,1) 100%);}
+          &:nth-child(6) {background: linear-gradient(90deg, rgb(150, 150, 165) 0%, rgba(35,29,77,1) 35%, rgba(59,41,144,1) 60%, rgba(189,112,103,1) 100%);}
         }
       }
     }
